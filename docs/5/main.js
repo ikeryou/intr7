e3cd24665ec841f9b928e16ff9c6c9ff130b3c17
ikeryou <ikeda.ryou@gmail.com>
2018-05-17 20:23:12 +0900

    Add sample5

---
+++ docs/5/main.js
@@ -0,0 +1,153 @@
+
+// マウス
+mouse = {
+  isDown:false,     // 画面押されてるか
+  x:0,              // マウス位置 X
+  y:0,              // マウス位置 Y
+  start:{x:0, y:0}, // マウスダウン時の座標
+  dist:{x:0, y:0}   // マウスダウンしてからの移動距離
+};
+
+// アニメーション用パラメータ
+param = {
+  x:0,
+  y:0,
+  d:0
+};
+
+// 文字
+moji = (window.navigator.platform.indexOf('Win') != -1) ? 'X' : '🍺';
+
+// 初期設定
+init();
+function init() {
+  $(window).on('mousemove', _eMouseMove).on('mousedown', _eMouseDown).on('mouseup', _eMouseUp);
+  update();
+}
+
+// 毎フレーム実行
+window.requestAnimationFrame(update);
+function update() {
+
+  if(mouse.isDown) {
+    // マウス押してるときは、マウスダウン時からのマウス移動量をちゃんと計算
+    dx = mouse.x - mouse.start.x;
+    dy = mouse.y - mouse.start.y;
+    mouse.dist.x = dx;
+    mouse.dist.y = dy;
+
+    // ##########################################################
+    var friction = 0.01; // 摩擦係数 小さいとより引っ張られる感じに
+    mouse.dist.x *= friction;
+    mouse.dist.y *= friction;
+    // ##########################################################
+
+  } else {
+    // マウス押してないときの移動量は0
+    mouse.dist.x = 0;
+    mouse.dist.y = 0;
+  }
+
+  // 滑らかに移動させるためイージングつける
+  var ease = 0.2; // イージング量 小さいとゆっくり
+  param.x += (mouse.dist.x - param.x) * ease;
+  param.y += (mouse.dist.y - param.y) * ease;
+
+  // 始点からの距離
+  param.d = Math.sqrt(param.x * param.x + param.y * param.y);
+
+  // 始点距離に対して文字数を決定
+  var hash = '';
+  var len = Math.min(50, ~~(param.d * 1));
+  for(var i = 0; i < len; i++) {
+    hash += moji;
+  }
+  location.hash = hash;
+  $('.js-tg').html(hash);
+
+  window.requestAnimationFrame(update);
+}
+
+
+// ----------------------------------------
+// イベント マウス動いた
+// ----------------------------------------
+function _eMouseMove(e) {
+
+  mouse.x = e.clientX;
+  mouse.y = e.clientY;
+
+}
+
+
+// ----------------------------------------
+// イベント マウス押した
+// ----------------------------------------
+function _eMouseDown(e) {
+
+  if(!mouse.isDown) {
+    mouse.isDown = true;
+    mouse.start.x = e.clientX;
+    mouse.start.y = e.clientY;
+  }
+
+}
+
+
+// ----------------------------------------
+// イベント マウス離した
+// ----------------------------------------
+function _eMouseUp(e) {
+
+  mouse.isDown = false;
+
+}
+
+
+
+
+// ########################################
+// ユーティリティ系 ↓
+// ########################################
+
+// ----------------------------------------
+// 度からラジアンに変換
+// @val : 度
+// ----------------------------------------
+function radian(val) {
+  return val * Math.PI / 180;
+}
+
+// ----------------------------------------
+// ラジアンから度に変換
+// @val : ラジアン
+// ----------------------------------------
+function degree(val) {
+  return val * 180 / Math.PI;
+}
+
+// ----------------------------------------
+// minからmaxまでランダム
+// ----------------------------------------
+function random(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+// ----------------------------------------
+// 範囲変換
+// @val     : 変換したい値
+// @toMin   : 変換後の最小値
+// @toMax   : 変換後の最大値
+// @fromMin : 変換前の最小値
+// @fromMax : 変換前の最大値
+// ----------------------------------------
+function map(val, toMin, toMax, fromMin, fromMax) {
+  if(val <= fromMin) {
+    return toMin;
+  }
+  if(val >= fromMax) {
+    return toMax;
+  }
+  p = (toMax - toMin) / (fromMax - fromMin);
+  return ((val - fromMin) * p) + toMin;
+}
